363dd02fab4e463da5523e07ba28b9ae000cefe8
Anuar <anuar.imashev@gmail.com>
2024-04-09 22:08:00 +0500

    updated entity

--- src/main/java/kz/singularity/adminpanel/models/ChiefDish.java
+++ src/main/java/kz/singularity/adminpanel/models/ChiefDish.java
@@ -1,10 +1,13 @@
 package kz.singularity.adminpanel.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -18,5 +21,8 @@ public class ChiefDish {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @ManyToMany(fetch = FetchType.EAGER)
+  private List<Dish> dishes;
 
 }
